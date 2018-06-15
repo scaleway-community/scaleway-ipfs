5001618f93b3c771ff9330add6217ce007b1745d
Maël Le Garrec <mlegarrec@online.net>
2018-06-15 11:26:37 +0200

    Initial commit
    Working IPFS image

---
+++ latest/env.mk
@@ -0,0 +1,7 @@
+IMAGE_NAME = ipfs
+IMAGE_VERSION = latest
+IMAGE_VERSION_ALIASES =	0.4.15
+IMAGE_TITLE = IPFS
+IMAGE_DESCRIPTION = "IPFS"
+IMAGE_SOURCE_URL = https://github.com/scaleway-community/scaleway-ipfs
+IMAGE_VENDOR_URL = https://ipfs.io/
